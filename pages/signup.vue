ce6ffd490d84c0f163cc1f2c2916a8392da4b317
albmg <albmguer@gmail.com>
2020-08-01 09:30:32 +0100

    initial commit

---
+++ pages/signup.vue
@@ -0,0 +1,74 @@
+<template>
+  <v-form v-model="valid">
+    <v-container>
+      <v-row>
+        <v-col
+          cols="12"
+          md="4"
+        >
+        <h2>Únete a eCambuy</h2>
+        <h4>Crea una cuenta para intercambiar tus productos ecológicos</h4>
+          <v-text-field
+            v-model="username"
+            :rules="nameRules"
+            :counter="10"
+            label="Name"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="lastname"
+            :rules="nameRules"
+            :counter="10"
+            label="Last name"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="email"
+            :rules="emailRules"
+            label="E-mail"
+            required
+          ></v-text-field>
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-spacer></v-spacer>    
+    <v-btn
+      color="primary"
+      nuxt
+      to="/productList"
+      >
+      Login
+      </v-btn>
+  </v-form>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      valid: false,
+      username: '',
+      lastname: '',
+      nameRules: [
+        v => !!v || 'Name is required',
+        v => v.length <= 10 || 'Name must be less than 10 characters',
+      ],
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+/.test(v) || 'E-mail must be valid',
+      ],
+    }),
+  }
+</script>
