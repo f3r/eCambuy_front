3e26bdbaf95e9f72a4cdc20aeda6b52dd93a0f58
albmg <albmguer@gmail.com>
2020-08-03 22:35:25 +0100

    fixing login form

--- pages/login.vue
+++ pages/login.vue
@@ -1,36 +1,43 @@
 <template>
-  <v-form v-model="valid">
-    <v-container>
-      <v-row>
-        <v-col cols="12" md="4">
+  <v-container>
+    <v-row>
+      <v-col cols="12" md="4" class="mx-auto">
+        <v-form v-model="valid">
+          <h2>Bienvenido</h2>
+          <h4>Inicia sesión para comprar y vender productos</h4>
           <v-text-field
             v-model="email"
             :rules="emailRules"
             label="E-mail"
             required
           ></v-text-field>
-        </v-col>
-        <v-col cols="12" md="4">
+
           <v-text-field
             v-model="password"
             :rules="passwordRules"
-            :counter="10"
+            type="password"
             label="password"
             required
           ></v-text-field>
-        </v-col>
-      </v-row>
-    </v-container>
-    <v-spacer></v-spacer>
-    <v-btn class="mb-3" color="primary" nuxt @click="login">
-      Enviar
-    </v-btn>
-    <v-divider></v-divider>
-    <p>No tienes cuenta? Crea una</p>
-    <v-btn color="primary" nuxt to="/signup">
-      signup
-    </v-btn>
-  </v-form>
+
+          <v-btn color="primary" nuxt @click="login">
+            Enviar
+          </v-btn>
+        </v-form>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" md="4" class="mx-auto pt-3">
+        <v-divider></v-divider>
+        <p class="mt-4">
+          No tienes cuenta? Crea una
+          <v-btn color="primary" text nuxt to="/signup">
+            signup
+          </v-btn>
+        </p>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
