dd6323b4ce70416a80a6bd0c2cc0fb51af0891ef
albmg <albmguer@gmail.com>
2020-08-04 01:20:47 +0100

    delete product working

--- pages/productList/_id.vue
+++ pages/productList/_id.vue
@@ -1,31 +1,53 @@
 <template>
   <v-main>
-    <v-card class="mx-auto" max-width="400">
-      <v-card-title>Vendedor:{{ product.owner }}</v-card-title>
-      <v-img class="white--text align-end" height="200px" :src="product.image">
-      </v-img>
-      <v-spacer></v-spacer>
-      <v-card-title class="pb-0">{{ product.name }}</v-card-title>
-      <v-spacer></v-spacer>
-      <v-card-subtitle class="pb-0"
-        >Precio: {{ product.price }}</v-card-subtitle
-      >
-      <v-card-subtitle class="pb-0"
-        >Municipio: {{ product.location }}</v-card-subtitle
-      >
-      <v-card-text class="text--primary">
-        <div>Descripción: {{ product.description }}</div>
-      </v-card-text>
-      <v-card-actions>
-        <v-btn color="orange" text to="/productList">
-          Lista de productos
-        </v-btn>
+    <v-col cols="12" md="8" class="mx-auto">
+      <v-card>
+        <v-card-title>Vendedor:{{ product.owner }}</v-card-title>
+        <v-img
+          class="white--text align-end"
+          height="200px"
+          :src="product.image"
+        >
+        </v-img>
         <v-spacer></v-spacer>
-        <v-btn color="info" text>
-          Chat
-        </v-btn>
-      </v-card-actions>
-    </v-card>
+        <v-card-title class="pb-0">{{ product.name }}</v-card-title>
+        <v-spacer></v-spacer>
+        <v-card-subtitle class="pb-0"
+          >Precio: {{ product.price }}</v-card-subtitle
+        >
+        <v-card-subtitle class="pb-0"
+          >Municipio: {{ product.location }}</v-card-subtitle
+        >
+        <v-card-text class="text--primary">
+          <div>Descripción: {{ product.description }}</div>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="success" text to="/productList">
+            Lista de productos
+          </v-btn>
+          <v-spacer></v-spacer>
+          <v-btn color="info" text>
+            Chat
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+    <v-col cols="12" md="4" class="mx-auto mt-3">
+      <v-card>
+        <div v-if="$auth.loggedIn">
+          <!--<div v-if="$auth.user === product.owner._id">-->
+          <v-btn class="mb-2" block color="orange" @click="editProduct"
+            >Editar Producto</v-btn
+          >
+
+          <v-btn block color="error" @click="deleteProduct"
+            >Borrar Producto</v-btn
+          >
+          <!--</div>-->
+        </div>
+        <div v-else>Hola amigo</div>
+      </v-card>
+    </v-col>
   </v-main>
 </template>
 
@@ -44,6 +66,14 @@ export default {
     async getProduct() {
       const response = await this.$axios.$get(`/products/${this.id}`)
       return response
+    },
+    editProduct() {
+      alert('con este edito')
+    },
+    async deleteProduct() {
+      await this.$axios.$delete(`/products/me/${this.id}`)
+      this.$router.push('/productList')
+      // alert('con este borro')
     },
   },
 }
